479cfadeffb19844222d5237d169ef6dc00aba0b
Gwen Lofman <gwen@lofman.co>
2016-07-14 16:04:22 -0400

    Updates document structure.
    All functions except for `initHeadline` are outside document load event.

--- js/headline.js
+++ js/headline.js
@@ -1,3 +1,34 @@
+function hideWord($word) {
+    var nextWord = takeNext($word);
+    switchWord($word, nextWord);
+    setTimeout(function(){ hideWord(nextWord) }, animationDelay);
+}
+
+function takeNext($word) {
+    return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+}
+
+function takePrev($word) {
+    return (!$word.is(':first-child')) ? $word.prev() : $word.parent().children().last();
+}
+
+function switchWord($oldWord, $newWord) {
+    $oldWord.removeClass('visible').addClass('hidden');
+    $newWord.removeClass('hidden').addClass('visible');
+}
+
+
+function loopAnimation($headlines) {
+    var duration = animationDelay;
+    $headlines.each(function(){
+        var headline = $(this);
+
+        //trigger animation
+        setTimeout(function(){ hideWord( headline.find('.visible').eq(0) ) }, duration);
+    });
+}
+
+
 jQuery(document).ready(function($){
     //set animation timing
     var animationDelay = 2500;
@@ -6,35 +37,7 @@ jQuery(document).ready(function($){
 
     function initHeadline() {
         //initialise headline animation
-        animateHeadline($('.headline'));
+        loopAnimation($('.headline'));
     }
 
-    function animateHeadline($headlines) {
-        var duration = animationDelay;
-        $headlines.each(function(){
-            var headline = $(this);
-
-            //trigger animation
-            setTimeout(function(){ hideWord( headline.find('.visible').eq(0) ) }, duration);
-        });
-    }
-
-    function hideWord($word) {
-        var nextWord = takeNext($word);
-        switchWord($word, nextWord);
-        setTimeout(function(){ hideWord(nextWord) }, animationDelay);
-    }
-
-    function takeNext($word) {
-        return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
-    }
-
-    function takePrev($word) {
-        return (!$word.is(':first-child')) ? $word.prev() : $word.parent().children().last();
-    }
-
-    function switchWord($oldWord, $newWord) {
-        $oldWord.removeClass('visible').addClass('hidden');
-        $newWord.removeClass('hidden').addClass('visible');
-    }
 });
